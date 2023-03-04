192c2ca644c18351c41a71155af5a8257fb8df41
klalicki <32229646+klalicki@users.noreply.github.com>
2023-03-04 10:45:51 -0500

    installs barba.js, gsap, and adds basic markup for page transitions

---
+++ src/assets/js/index.js
@@ -0,0 +1,22 @@
+import barba from "@barba/core";
+import { gsap } from "gsap";
+
+barba.init({
+  transitions: [
+    {
+      name: "opacity-transition",
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0,
+          x: 10,
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0,
+          x: 10,
+        });
+      },
+    },
+  ],
+});
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/assets/js/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  watch: true,
+};
