f18d817a35e2fa7e537be34dfb47f6746329ac8f
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-03-03 20:09:45 -0500

    fixes bug where different pages have different widths

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   // Return your Object options:
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/img");
+
+  // adds custom collections for projects and pages which are sorted by an 'order' parameter in front matter.
   eleventyConfig.addCollection("projectSorted", (collection) =>
     collection.getFilteredByTags("project").sort((a, b) => {
       return (a.data.order || 0) - (b.data.order || 0);
