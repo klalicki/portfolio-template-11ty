68003bd203db724bbc2f95de60383d61697aee38
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2024-03-14 00:39:38 -0400

    assorted a11y fixes

--- script-src/index.js
+++ script-src/index.js
@@ -15,9 +15,10 @@ const setNewMenuItem = (targetItem) => {
   });
   console.log(item);
 };
+//sidebar-nav-link-section nav-link-section-hidden
 const menuButtonSelector = ".btn-toggle-menu";
-const menuPanelSelector = ".main-nav";
-const classToToggle = "main-nav-hidden";
+const menuPanelSelector = ".sidebar-nav-link-section";
+const classToToggle = "nav-link-section-hidden";
 
 const toggleHandler = () => {
   document.querySelector(menuPanelSelector).classList.toggle(classToToggle);
@@ -31,7 +32,6 @@ const hideMenu = () => {
 
 barba.use(barbaCss);
 barba.init({
-  
   transitions: [
     {
       name: "fade",
